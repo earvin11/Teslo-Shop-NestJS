52c44fcd2a0443ce167ee469a4d45ce60b88d674
Earvin Gomez <earvinsantiago.11@gmail.com>
2023-01-20 16:24:08 -0400

    WebSocket

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,21 @@
+import { WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
+
+  handleConnection( client: Socket ) {
+    console.log('Cliente conectado: ', client.id);
+  }
+
+  handleDisconnect( client: Socket ) {
+    console.log('Cliente desconectado: ', client.id);
+  }
+
+
+}
